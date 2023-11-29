6dacf00eb0c2d486c707f372a8f423a357f4f326
Ulf Jährig <1982862+ujaehrig@users.noreply.github.com>
2023-11-29 12:33:41 +0100

    Update .gitignore and setup script
    Added `user.db` to `.gitignore` to prevent uploading sensitive user database to the repository. Additionally, changed the crontab entry in `setup.sh` to point to a python script (`catcher.py`) instead of a shell script (`catcher.sh`), reflecting the transition to a Python-based implementation for better maintainability and scalability.

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ sqlite3 user.db < setup.sql
 
 test -f setup-user.sql && sqlite3 user.db < setup-user.sql
 
-ctab="30 7 * * 1-5	cd ${PWD} ; ./catcher.sh"
-(crontab -u $(whoami) -l; echo "${ctab}" ) | crontab -u $(whoami) -
+ctab="30 7 * * 1-5	cd ${PWD} ; ./catcher.py"
+(crontab -u "$(whoami)" -l; echo "${ctab}" ) | crontab -u "$(whoami)" -
