b425563ef409553932e9f4c3e3a1ab767f2f88f3
Ulf Jährig <1982862+ujaehrig@users.noreply.github.com>
2023-11-29 09:24:38 +0100

    Refactor code for handling database & error checks
    Changes have been made to improve code readability and error handling.
    The database file name was corrected to 'user.db'.
    The 'isHoliday' function was renamed to 'is_holiday' as per Python naming conventions, and exception handling was added.
    The update query was altered to improve efficiency, and was moved inside the 'main' function for better encapsulation.
    The post request now includes JSON serialization for the data and the database connection is properly closed.

--- catcher.py
+++ catcher.py
@@ -1,48 +1,54 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 import configparser
 import json
 import requests
+import sqlite3
 
 config = configparser.ConfigParser()
-config.read('catcher.ini')
+config.read('config.ini')
 
 
-def isHoliday():
-    response = requests.get('https://date.nager.at/Api/v2/IsTodayPublicHoliday/DE')
-    return response.status_code == 200
+def is_holiday():
+    try:
+        response = requests.get('https://date.nager.at/Api/v2/IsTodayPublicHoliday/DE')
+        return response.status_code == 200
+    except:
+        return False
 
 
 def main():
-    if isHoliday:
+    if is_holiday():
         return
 
-
-    conn = sqlite3.connect("users.db")
+    conn = sqlite3.connect("user.db")
     cur = conn.cursor()
 
     cur.execute("""
-    	create temp table x(mail text);
-        insert into x 
-            select mail 
+          select mail 
             from user 
-            where weekdays like strftime('%%%w%%','now')
-            and ((vacation_start is null or vacation_end is null) 
-                or (date() < vacation_start or date() > vacation_end))
-            order by last_chosen asc
-            limit 1;
-        update user set last_chosen = date() where mail = (select mail from x);
-        select mail from x;
+           where weekdays like strftime('%%%w%%','now')
+             and ((vacation_start is null or vacation_end is null) 
+                 or (date() < vacation_start or date() > vacation_end))
+        order by last_chosen asc
+           limit 1
     """)
 
-    mail = cur.fetchone()
-    if mail is None:
+    result = cur.fetchone()
+    if result is None:
         return
 
-    data = { 'uid': mail }
-    headers = {'Content-type': 'application/json' }
-    response = requests.post(config.get('slack', 'webhook')), headers=headers, data = data)
+    mail = result[0]
+    cur.execute("""
+         update user set last_chosen = date() where mail = ?
+    """, result)
+    conn.commit()
 
+    data = {'uid': mail}
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(config.get('slack', 'webhook'), headers=headers, data=json.dumps(data))
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
