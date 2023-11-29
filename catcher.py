49d81cd3aab1e7cb37301edcad09e04a8f0cfe8d
Ulf Jährig <1982862+ujaehrig@users.noreply.github.com>
2023-11-29 12:34:32 +0100

    Modify catcher.py, add comments and refactor functions
    Enhanced error handling and improved code readability in `catcher.py`.
    Added explanatory comments and precise exceptions, and refactored methods for cleaner segregation of responsibilities to enhance maintainability.
    A timeout was added to the request for better performance.
    The `main` function now checks if it is a holiday before continuing execution, restricting notifications on public holidays.

--- catcher.py
+++ catcher.py
@@ -10,17 +10,52 @@ config.read('config.ini')
 
 
 def is_holiday():
+    """
+    Checks if today is a public holiday in Germany.
+
+    :return: True if today is a public holiday, False otherwise
+    """
     try:
-        response = requests.get('https://date.nager.at/Api/v2/IsTodayPublicHoliday/DE')
+        response = requests.get('https://date.nager.at/Api/v2/IsTodayPublicHoliday/DE', timeout=0.1)
         return response.status_code == 200
-    except:
+    except requests.exceptions.RequestException:
         return False
 
 
-def main():
-    if is_holiday():
-        return
+def trigger_slack(mail):
+    """
+    :param mail: The email address of the user to be notified on Slack.
+    :type mail: str
+    :return: None
+    :rtype: None
 
+    This method triggers a Slack notification for the specified user using their email address. It sends a POST request to the configured Slack webhook with the email address as the payload.
+
+    Example usage:
+
+    ```
+    trigger_slack('user@example.com')
+    ```
+
+    Note: This method requires the `config` object to be properly configured with the Slack webhook URL.
+    """
+    try:
+        data = {'uid': mail}
+        headers = {'Content-type': 'application/json'}
+        requests.post(config.get('slack', 'webhook'), headers=headers, data=json.dumps(data), timeout=1)
+    except requests.exceptions.RequestException:
+        print("request to slack failed")
+
+
+def find_next_catcher():
+    """
+    This method `find_next_catcher` is used to retrieve the email address
+    of the next user who is available.
+    The method retrieves the email address from a database table based
+    on specific conditions.
+
+    :return: The email address of the next available user or None
+    """
     conn = sqlite3.connect("user.db")
     cur = conn.cursor()
 
@@ -35,20 +70,21 @@ def main():
     """)
 
     result = cur.fetchone()
-    if result is None:
-        return
-
-    mail = result[0]
-    cur.execute("""
-         update user set last_chosen = date() where mail = ?
-    """, result)
-    conn.commit()
-
-    data = {'uid': mail}
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(config.get('slack', 'webhook'), headers=headers, data=json.dumps(data))
+    if result is not None:
+        cur.execute("update user set last_chosen = date() where mail = ?", result)
+        conn.commit()
 
     conn.close()
+    return None if result is None else result[0]
+
+
+def main():
+    if is_holiday():
+        return
+
+    mail = find_next_catcher()
+    trigger_slack(mail)
+
 
 if __name__ == "__main__":
     main()
